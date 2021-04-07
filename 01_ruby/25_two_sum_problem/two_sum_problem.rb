52473965f3bab0df4da97c8cda2f654bcc1a2f8c
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-04-07 22:48:30 +0300

    Finish Two Sum Problem

---
+++ 01_ruby/25_two_sum_problem/two_sum_problem.rb
@@ -0,0 +1,36 @@
+# Brute force O(n^2)
+def bad_two_sum?(arr, target_sum)
+  (0...arr.length - 1).each do |idx_1|
+    (idx_1 + 1...arr.length).each do |idx_2|
+      return true if arr[idx_1] + arr[idx_2] == target_sum
+    end
+  end
+  false
+end
+
+# Sorting O(nlogn)
+def okay_two_sum?(arr, target_sum)
+  sorted_arr = arr.sort
+  start_idx = 0
+  end_idx = arr.length - 1
+
+  until start_idx == end_idx
+    sum = sorted_arr[start_idx] + sorted_arr[end_idx]
+    return true if sum == target_sum
+    start_idx += 1 if sum < target_sum
+    end_idx -= 1 if sum > target_sum
+  end
+
+  false
+end
+
+# Hash Map O(n)
+def two_sum?(arr, target_sum)
+  integers = Hash.new(false)
+  arr.each { |num| integers[num] = true }
+  
+  arr.any? do |num|
+    remainder = target_sum - num
+    remainder != num && integers[remainder]
+  end
+end

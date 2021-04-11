e25065e028e249ecd1c942da991b6f60c6fa6035
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-04-11 19:08:21 +0300

    Add belongs_to associations to Course model

--- 02_sql/05_associations_exercise/associations_exercise/app/models/course.rb
+++ 02_sql/05_associations_exercise/associations_exercise/app/models/course.rb
@@ -12,4 +12,6 @@
 class Course < ApplicationRecord
     has_many :enrollments
     has_many :enrolled_students, through: :enrollments, source: :student
+    belongs_to :prerequisite, class_name: 'Course', foreign_key: :prereq_id
+    belongs_to :instructor, class_name: 'User'
 end

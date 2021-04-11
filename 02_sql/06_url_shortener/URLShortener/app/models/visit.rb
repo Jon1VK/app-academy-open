b04eeff37a9eb83ef0b80044074e701839ee0dbf
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-04-11 22:34:06 +0300

    Add schema annotations

--- 02_sql/06_url_shortener/URLShortener/app/models/visit.rb
+++ 02_sql/06_url_shortener/URLShortener/app/models/visit.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: visits
+#
+#  id               :bigint           not null, primary key
+#  user_id          :bigint           not null
+#  shortened_url_id :bigint           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class Visit < ApplicationRecord
   belongs_to :user
 

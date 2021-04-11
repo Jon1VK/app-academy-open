b587c199c488abab447bda8fbd4b0d60653730f1
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-04-11 19:12:40 +0300

    Add optional: true option for :prerequisite association

--- 02_sql/05_associations_exercise/associations_exercise/app/models/course.rb
+++ 02_sql/05_associations_exercise/associations_exercise/app/models/course.rb
@@ -12,6 +12,10 @@
 class Course < ApplicationRecord
     has_many :enrollments
     has_many :enrolled_students, through: :enrollments, source: :student
-    belongs_to :prerequisite, class_name: 'Course', foreign_key: :prereq_id
+    belongs_to :prerequisite,
+        class_name: 'Course',
+        foreign_key: :prereq_id,
+        optional: true
+
     belongs_to :instructor, class_name: 'User'
 end

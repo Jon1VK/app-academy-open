8fc82cc789887f4e4e31a9b41349670a637d3049
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-04-21 10:53:21 +0300

    Create responses table and skeleton for Response model

--- 02_sql/10_polls/polls_app/db/schema.rb
+++ 02_sql/10_polls/polls_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_074423) do
+ActiveRecord::Schema.define(version: 2021_04_21_075139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2021_04_21_074423) do
     t.index ["poll_id"], name: "index_questions_on_poll_id"
   end
 
+  create_table "responses", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "answer_choice_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["answer_choice_id"], name: "index_responses_on_answer_choice_id"
+    t.index ["user_id"], name: "index_responses_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -49,4 +58,6 @@ ActiveRecord::Schema.define(version: 2021_04_21_074423) do
   add_foreign_key "answer_choices", "questions"
   add_foreign_key "polls", "users"
   add_foreign_key "questions", "polls"
+  add_foreign_key "responses", "answer_choices"
+  add_foreign_key "responses", "users"
 end

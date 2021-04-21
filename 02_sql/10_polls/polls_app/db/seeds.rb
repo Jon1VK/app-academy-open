508b574d910cf4bc445d785732f213d71ab46c97
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-04-21 11:10:08 +0300

    Add seeds for db

--- 02_sql/10_polls/polls_app/db/seeds.rb
+++ 02_sql/10_polls/polls_app/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create!(username: 'Joni Vainio-Kaila')
+u2 = User.create!(username: 'Elli Esimerkki')
+u3 = User.create!(username: 'Matti Mallikas')
+
+p1 = Poll.create!(title: 'Poll 1: Joni', user: u1)
+p2 = Poll.create!(title: 'Poll 2: Joni', user: u1)
+p3 = Poll.create!(title: 'Poll 3: Elli', user: u2)
+
+q1 = Question.create!(text: 'Question 1: Poll 1', poll: p1)
+q2 = Question.create!(text: 'Question 2: Poll 1', poll: p1)
+q3 = Question.create!(text: 'Question 3: Poll 3', poll: p3)
+
+a1 = AnswerChoice.create!(text: 'Answer 1: Question 1', question: q1)
+a2 = AnswerChoice.create!(text: 'Answer 2: Question 1', question: q1)
+a3 = AnswerChoice.create!(text: 'Answer 3: Question 1', question: q1)
+
+r1 = Response.create!(user: u3, answer_choice: a1)

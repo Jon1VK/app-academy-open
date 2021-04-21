bcf1ae50e6b7f7ea4a35b0bca5b727de19308784
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-04-21 11:52:20 +0300

    Add few associations and a custom validation

--- 02_sql/10_polls/polls_app/app/models/response.rb
+++ 02_sql/10_polls/polls_app/app/models/response.rb
@@ -9,9 +9,30 @@
 #  updated_at       :datetime         not null
 #
 class Response < ApplicationRecord
+  validate :respondent_has_not_answered_yet
+
   belongs_to :respondent,
     class_name: 'User',
     foreign_key: :user_id
 
   belongs_to :answer_choice
+
+  has_one :question,
+    through: :answer_choice
+
+  def sibling_responses
+    self.question
+      .responses
+      .where.not(id: self.id)
+  end
+
+  def respondent_already_answered?
+    self.sibling_responses.exists?(respondent: self.user_id)
+  end
+
+  def respondent_has_not_answered_yet
+    if self.respondent_already_answered?
+      errors.add(:base, 'User has alreaydy answered')
+    end
+  end
 end

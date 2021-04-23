23fd22f899dbc99cfc25d39170ffeaa627e80fec
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-04-23 23:11:44 +0300

    Finish UsersController

--- 02_sql/13_first_routes_and_controllers/first_routes_and_controllers/app/controllers/users_controller.rb
+++ 02_sql/13_first_routes_and_controllers/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,21 +1,40 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I'm in the index action!"
+    render json: User.all
   end
 
   def create
-    render json: params
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
-    render json: params
+    render json: User.find(params[:id])
   end
 
-  def create
-    render json: params
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    render json: params
+    user = User.find(params[:id])
+
+    render json: user.destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end

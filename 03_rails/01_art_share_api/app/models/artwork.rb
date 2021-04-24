c991749340c178f85b8cad6b7e3d5ac83331f756
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-04-24 11:37:22 +0300

    Add dependent callbacks

--- 03_rails/01_art_share_api/app/models/artwork.rb
+++ 03_rails/01_art_share_api/app/models/artwork.rb
@@ -19,7 +19,8 @@ class Artwork < ApplicationRecord
   belongs_to :artist,
     class_name: 'User'
 
-  has_many :artwork_shares
+  has_many :artwork_shares,
+    dependent: :destroy
 
   has_many :shared_viewers,
     through: :artwork_shares,

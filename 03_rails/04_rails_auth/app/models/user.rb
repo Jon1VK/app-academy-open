80b213270303f482e3fa2702faa9a6c791c2b688
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-05-03 16:47:44 +0300

    Test out rails has_secure_password authentication

---
+++ 03_rails/04_rails_auth/app/models/user.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }
+end

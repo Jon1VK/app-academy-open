80b59b284a2eaa36f632352f9c24e74bfe611e76
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-08-14 12:49:09 +0300

    Create User model

---
+++ 03_rails/06_goal_app/spec/models/user_spec.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { FactoryBot.build(:user) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_length_of(:password).is_at_least(6) }
+
+    context "when loaded from a db" do
+      subject do
+        FactoryBot.create(:user)
+        User.first
+      end
+
+      it 'is expected to have :password set to <nil> and to be still valid' do
+        expect(subject.password).to be_nil
+        expect(subject).to be_valid
+      end
+    end
+  end
+end

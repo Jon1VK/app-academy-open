1c8c2143b116810643193a55144fa413d891b4df
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-08-14 21:11:21 +0300

    Install annotate gem and annotate models

--- 03_rails/06_goal_app/spec/models/user_spec.rb
+++ 03_rails/06_goal_app/spec/models/user_spec.rb
@@ -3,10 +3,14 @@
 # Table name: users
 #
 #  id              :bigint           not null, primary key
-#  username        :string           not null
 #  password_digest :string           not null
+#  username        :string           not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_username  (username) UNIQUE
 #
 require 'rails_helper'
 

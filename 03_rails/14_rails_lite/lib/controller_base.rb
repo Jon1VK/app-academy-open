f32625aa49980ed0450f31777952b456cebdf679
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-08-20 21:51:19 +0300

    Finish Phase 5 and 6

--- 03_rails/14_rails_lite/lib/controller_base.rb
+++ 03_rails/14_rails_lite/lib/controller_base.rb
@@ -7,9 +7,10 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, params={})
     @req = req
     @res = res
+    @params = req.params.merge(params)
   end
 
   # Helper method to alias @already_built_response
@@ -21,7 +22,7 @@ class ControllerBase
   def redirect_to(url)
     raise 'Render or redirect was already called' if already_built_response?
     res.redirect(url)
-    @session.store_session(res)
+    session.store_session(res)
     @already_built_response = true
   end
 
@@ -32,7 +33,7 @@ class ControllerBase
     raise 'Render or redirect was already called' if already_built_response?
     res.write(content)
     res.content_type = content_type
-    @session.store_session(res)
+    session.store_session(res)
     @already_built_response = true
   end
 
@@ -55,6 +56,8 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
   end
 end
 

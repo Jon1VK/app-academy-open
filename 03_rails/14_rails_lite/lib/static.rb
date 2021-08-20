0698846b17495bd4767dd01f399d536ec3b17065
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-08-21 00:27:39 +0300

    Finish Phase IX

--- 03_rails/14_rails_lite/lib/static.rb
+++ 03_rails/14_rails_lite/lib/static.rb
@@ -1,7 +1,34 @@
+require 'mime/types'
+
 class Static
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    req = Rack::Request.new(env)
+
+    if req.path.start_with?('/public/')
+      res = Rack::Response.new
+
+      file_dir = File.dirname(req.path[1..-1])
+      file_name = File.basename(req.path)
+      file_uri = File.join(file_dir, file_name)
+
+      if File.file?(file_uri)
+        file = File.read(file_uri)
+        res.status = 200
+        res.write(file)
+        res.content_type = MIME::Types.type_for(file_uri).first.content_type
+        res.finish
+      else
+        res.status = 404
+        res.finish
+      end
+    else
+      app.call(env)
+    end
   end
 end

38bf96b2f7abee0f77d11a1664dbb2068265a2da
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-08-20 17:18:20 +0300

    Finish Phase II

--- 03_rails/14_rails_lite/lib/controller_base.rb
+++ 03_rails/14_rails_lite/lib/controller_base.rb
@@ -8,20 +8,30 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise 'Render or redirect was already called' if already_built_response?
+    res.redirect(url)
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise 'Render or redirect was already called' if already_built_response?
+    res.write(content)
+    res.content_type = content_type
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates

14e878ff681db4361f7e15b3f7c5c3b0d8dbb5e6
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-08-30 09:20:42 +0300

    Finish jQuery Lite

--- 04_javascript/19_jquery_lite/src/index.js
+++ 04_javascript/19_jquery_lite/src/index.js
@@ -26,5 +26,59 @@ function jQueryLite(arg) {
   throw new Error(`Invalid argument ${arg}`);
 }
 
+jQueryLite.extend = (target, ...sources) => {
+  sources.forEach((source) => Object.assign(target, source));
+  return target;
+};
+
+jQueryLite.ajax = (options) => {
+  const defaults = {
+    url: 'index.html',
+    type: 'get',
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    data: {},
+  };
+
+  options = jQueryLite.extend(defaults, options);
+
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(options.type, options.url);
+
+    xhr.onload = () => {
+      if (xhr.status !== 0) {
+        const response = JSON.parse(xhr.response);
+        if (options.success) {
+          options.success(response);
+        }
+
+        resolve(response);
+      } else {
+        if (options.error) {
+          options.error();
+        }
+
+        reject({
+          status: xhr.status,
+          statusText: xhr.statusText,
+        });
+      }
+    };
+
+    xhr.onerror = () => {
+      if (options.error) {
+        options.error();
+      }
+
+      reject({
+        status: xhr.status,
+        statusText: xhr.statusText,
+      });
+    };
+
+    xhr.send(options.data);
+  });
+};
+
 window.jQueryLite = jQueryLite;
 window.$l = jQueryLite;

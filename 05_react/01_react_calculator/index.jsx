4fdb4fb3d1a2a9ec219b2351bb42b4447af8a640
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-08-31 18:15:21 +0300

    Initial commit for React Calculator

---
+++ 05_react/01_react_calculator/index.jsx
@@ -0,0 +1,4 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+
+ReactDom.render(<h1>Hello, World!</h1>, document.getElementById('app'));
---
+++ 05_react/01_react_calculator/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'index.jsx'),
+  watch: true,
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/env', '@babel/react'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
+};

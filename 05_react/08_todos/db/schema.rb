606d79ecedf36a8b993bbd7e668f88c5165fa183
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-09-09 20:57:56 +0300

    Create steps table

--- 05_react/08_todos/db/schema.rb
+++ 05_react/08_todos/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_115408) do
+ActiveRecord::Schema.define(version: 2021_09_09_175639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "steps", force: :cascade do |t|
+    t.string "title", null: false
+    t.boolean "done", default: false
+    t.bigint "todo_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["todo_id"], name: "index_steps_on_todo_id"
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "title", null: false
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_09_07_115408) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "steps", "todos"
 end

5f7934bac1602f141b0fd893d05cf82d245eb203
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-09-13 14:54:37 +0300

    Create tag and tagging models

---
+++ 05_react/08_todos/app/models/tagging.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: taggings
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  tag_id     :bigint           not null
+#  todo_id    :bigint           not null
+#
+# Indexes
+#
+#  index_taggings_on_tag_id   (tag_id)
+#  index_taggings_on_todo_id  (todo_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (tag_id => tags.id)
+#  fk_rails_...  (todo_id => todos.id)
+#
+class Tagging < ApplicationRecord
+  belongs_to :todo
+  belongs_to :tag
+end

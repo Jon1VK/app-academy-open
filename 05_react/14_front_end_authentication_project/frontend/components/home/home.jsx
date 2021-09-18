28fea50e888d2d27df3d89bca73dd8713daf04c1
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-09-18 07:58:37 +0300

    Initial commit for Front End authentication project

---
+++ 05_react/14_front_end_authentication_project/frontend/components/home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => (
+  <div className="home">
+    <div className="hero-img-frame">
+      <img
+        className="hero-img"
+        src="https://i.pinimg.com/originals/a3/05/cf/a305cf391ea73fe7dadbdde19a5e0d3d.jpg"
+      />
+    </div>
+    <h1>Where birds can chirp.</h1>
+  </div>
+);
